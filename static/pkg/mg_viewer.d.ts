99baedbaaba3aa7943f3c7bf067f30ec75be6424
Michael Goodale <michael.goodale@mail.mcgill.ca>
2025-04-11 17:11:11 +0200

    New mg version

--- static/pkg/mg_viewer.d.ts
+++ static/pkg/mg_viewer.d.ts
@@ -1,23 +1,16 @@
 /* tslint:disable */
 /* eslint-disable */
-/**
-* @param {string} sentence
-* @param {string} grammar
-* @param {string} target_category
-* @param {number} min_log_prob
-* @param {number} max_steps
-* @param {number} max_beams
-* @returns {any}
-*/
 export function get_parse(sentence: string, grammar: string, target_category: string, min_log_prob: number, max_steps: number, max_beams: number): any;
 
 export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
 
 export interface InitOutput {
   readonly memory: WebAssembly.Memory;
-  readonly get_parse: (a: number, b: number, c: number, d: number, e: number, f: number, g: number, h: number) => number;
+  readonly get_parse: (a: number, b: number, c: number, d: number, e: number, f: number, g: number, h: number, i: number) => any;
+  readonly __wbindgen_export_0: WebAssembly.Table;
   readonly __wbindgen_malloc: (a: number, b: number) => number;
   readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+  readonly __wbindgen_start: () => void;
 }
 
 export type SyncInitInput = BufferSource | WebAssembly.Module;
@@ -25,18 +18,18 @@ export type SyncInitInput = BufferSource | WebAssembly.Module;
 * Instantiates the given `module`, which can either be bytes or
 * a precompiled `WebAssembly.Module`.
 *
-* @param {SyncInitInput} module
+* @param {{ module: SyncInitInput }} module - Passing `SyncInitInput` directly is deprecated.
 *
 * @returns {InitOutput}
 */
-export function initSync(module: SyncInitInput): InitOutput;
+export function initSync(module: { module: SyncInitInput } | SyncInitInput): InitOutput;
 
 /**
 * If `module_or_path` is {RequestInfo} or {URL}, makes a request and
 * for everything else, calls `WebAssembly.instantiate` directly.
 *
-* @param {InitInput | Promise<InitInput>} module_or_path
+* @param {{ module_or_path: InitInput | Promise<InitInput> }} module_or_path - Passing `InitInput` directly is deprecated.
 *
 * @returns {Promise<InitOutput>}
 */
-export default function __wbg_init (module_or_path?: InitInput | Promise<InitInput>): Promise<InitOutput>;
+export default function __wbg_init (module_or_path?: { module_or_path: InitInput | Promise<InitInput> } | InitInput | Promise<InitInput>): Promise<InitOutput>;
--- static/pkg/mg_viewer_bg.wasm.d.ts
+++ static/pkg/mg_viewer_bg.wasm.d.ts
@@ -1,6 +1,8 @@
 /* tslint:disable */
 /* eslint-disable */
 export const memory: WebAssembly.Memory;
-export function get_parse(a: number, b: number, c: number, d: number, e: number, f: number, g: number, h: number): number;
-export function __wbindgen_malloc(a: number, b: number): number;
-export function __wbindgen_realloc(a: number, b: number, c: number, d: number): number;
+export const get_parse: (a: number, b: number, c: number, d: number, e: number, f: number, g: number, h: number, i: number) => any;
+export const __wbindgen_export_0: WebAssembly.Table;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_start: () => void;
